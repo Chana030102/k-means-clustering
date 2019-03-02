2114027a225719e4a9fc64af79498e7e4a71d66a
Chana030102 <chanaar@pdx.edu>
2019-03-01 16:49:44 -0800

    Added centroid calculation

--- kmeans.py
+++ kmeans.py
@@ -6,6 +6,8 @@
 
 import numpy as np
 
+MAX = 16
+
 class Cluster:
 
     # K = number of clusers
@@ -14,7 +16,7 @@ class Cluster:
         # Initialize K centers with random values
         # row index = center index
         # col index = attribute index
-        self.centers = np.random.rand(K,num_attr)
+        self.centers = np.random.rand(K,num_attr)*MAX
         
         self.amse = 0     # average mean square error
         self.mss  = 0     # mean square separation
@@ -23,11 +25,13 @@ class Cluster:
     # Fit cluster centers to training data
     # Done calculating when centers don't move 
     def fit(self, data, label):
-        prev_center = self.centers
-        new_center  = np.zeros(self.centers.shape)
+        new_center  = self.centers
+        prev_center = np.zeros(self.centers.shape)
         distance    = np.zeros((len(self.centers),len(data)))
         
         while(prev_center != new_center):
+            prev_center = new_center
+            
             # Calculate distance from center to each data instance
             for i in range(len(self.centers)):
                 d = data - self.centers[i]
@@ -38,3 +42,16 @@ class Cluster:
 
             # Identify which center data instances are closest to
             dd = distance.transpose()
+            cindex = dd.argmin(axis=1)
+
+            # Calculate new centers
+            for i in range(len(self.centers)):
+                index = np.where(cindex==i)
+                index = index.reshape(-1)
+                cdata = data[index]
+
+                cdata = np.sum(cdata,axis=0)/len(index)
+                new_center[i] = cdata
+            
+
+                
