21734946476ee64fc82cf06f2c812f8ea5c6ceb3
Chana030102 <chanaar@pdx.edu>
2019-03-05 19:57:13 -0800

    Add center calculation and WIP cluster classifying

--- kmeans.py
+++ kmeans.py
@@ -16,42 +16,75 @@ class Cluster:
         # Initialize K centers with random values
         # row index = center index
         # col index = attribute index
-        self.centers = np.random.rand(K,num_attr)*MAX
-        
+        self.centers = np.random.randint(0,MAX+1,size=(K,num_attr))
+        self.classes = np.zeros(K)
+
         self.amse = 0     # average mean square error
         self.mss  = 0     # mean square separation
         self.me   = 0     # mean entropy
 
+    # Calculate distance between data and cluster centers
+    # return_raw:
+    #   False --> return index of centers that each instance belongs to
+    #   True  --> return distances between each instance and each center
+    def distance(self,data,return_raw=False):
+        distance = np.zeros((len(self.centers),len(data)))
+
+         # Calculate distance from center to each data instance
+        for i in range(len(self.centers)):
+            d = data - self.centers[i]
+            d = np.square(d)
+            d = np.sum(d,axis=1) 
+            distance[i] = d
+        distance = np.sqrt(distance)
+
+        if return_raw == False:
+            # Identify which center data instances are closest to
+            dd = distance.transpose()
+            return dd.argmin(axis=1)
+        else:
+            return distance
+
     # Fit cluster centers to training data
-    # Done calculating when centers don't move 
+    # Done calculating when centers don't move  
     def fit(self, data, label):
         new_center  = self.centers
         prev_center = np.zeros(self.centers.shape)
-        distance    = np.zeros((len(self.centers),len(data)))
         
-        while(prev_center != new_center):
+        while(not np.array_equiv(prev_center,new_center)):
             prev_center = new_center
-            
-            # Calculate distance from center to each data instance
-            for i in range(len(self.centers)):
-                d = data - self.centers[i]
-                d = np.square(d)
-                d = np.sum(d,axis=1) 
-                distance[i] = d
-            distance = np.sqrt(distance)
-
-            # Identify which center data instances are closest to
-            dd = distance.transpose()
-            cindex = dd.argmin(axis=1)
+            cindex = self.distance(data)
 
             # Calculate new centers
             for i in range(len(self.centers)):
-                index = np.where(cindex==i)
+                index = np.asarray(np.where(cindex==i))
                 index = index.reshape(-1)
-                cdata = data[index]
 
-                cdata = np.sum(cdata,axis=0)/len(index)
+                if index.size == 0: 
+                    # randomly choose new centroid when cluster is empty
+                    cdata = np.random.randint(0,MAX+1,size=self.centers.shape[1])
+                else:
+                    # calculate new position of cluster centroid
+                    cdata = data[index]
+                    cdata = np.sum(cdata,axis=0)/len(index)
+
                 new_center[i] = cdata
+        self.centers = new_center
+
+        # Classify the clusters
+        # The most frequent class in the cluster is the class of the cluster
+        print("Classifying clusters")
+        for i in range(len(self.centers)):
+            index = np.asarray(np.where(cindex==i))
+            index = index.reshape(-1)
+
+            u, indices = np.unique(label[index], return_inverse=True)
+            self.classes[i] = u[np.argmax(np.bincount(indices))]
+
+        
+
+
+            
             
 
                 
--- main.py
+++ main.py
@@ -5,10 +5,13 @@
 # Homework 5 - K-Means Clustering
 
 import numpy as np
+import kmeans as k
 
 TRAIN_FILE = "../optdigits/optdigits.train"
 TEST_FILE  = "../optdigits/optdigits.test"
 DELIMITER  = ","
+
+K = 10
 
 #===== Data Import ====
 traind = np.loadtxt(TRAIN_FILE,delimiter=DELIMITER)
@@ -19,3 +22,5 @@ testd = np.loadtxt(TEST_FILE,delimiter=DELIMITER)
 testl = testd[:,-1]
 testd = np.delete(testd,-1,axis=1)
 
+cluster = k.Cluster(K,traind.shape[1])
+cluster.fit(traind,trainl)
